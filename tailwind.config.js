940a63a038aaaa8ea212346b21d2b3e990ccbd2f
Ivan Metzger <ivan.metzger@gsa.gov>
2023-05-01 07:11:16 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-    purge: ['./layouts/**/*.html'],
+    content: ['./layouts/**/*.html', './src/**/*.js'], // Add any other paths necessary for your project
     darkMode: false,
     theme: {
       extend: {},
     },
     variants: {
-      extend: {},
+      extend: {
+        backgroundColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],
+        borderColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],
+        textColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],
+        // Add any other utilities you want to extend with all variants
+      },
     },
     plugins: [
       require('@tailwindcss/forms'),
