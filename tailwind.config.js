9bdc3b64b9c0e5ea98491ba5d7d40d6dc78b17c2
Ivan Metzger <ivan.metzger@gsa.gov>
2023-04-30 20:04:18 -0400

    postcss successful build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: ['./layouts/**/*.html'],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
